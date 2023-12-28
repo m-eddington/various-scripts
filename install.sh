e780c8a99afb3cf82961d0f7ff8853ee7456e3c0
m-eddington <no-email@noemail.no.email.no>
2023-12-28 09:52:05 +0100

    Changed to netbeans 20, added apt clean

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install default-jdk -y
 wget https://dlcdn.apache.org/netbeans/netbeans/20/netbeans-20-bin.zip
-if sha512sum netbeans-18-bin.zip | grep '59eb2b0e7887f0d962d180c4004a81cf752f12a2b9456c59417daf90a312877f697500580c53566a220bde2a49df2b95ba1e4dedb4cf59b49bdacdc47c98745f'
+if sha512sum netbeans-20-bin.zip | grep '59eb2b0e7887f0d962d180c4004a81cf752f12a2b9456c59417daf90a312877f697500580c53566a220bde2a49df2b95ba1e4dedb4cf59b49bdacdc47c98745f'
 then
 unzip netbeans-20-bin.zip
 sudo mv netbeans/ /opt/
@@ -22,6 +22,7 @@ echo 'Comment=Netbeans' >> /home/$USER/.local/share/applications/netbeans.deskto
 echo 'Icon=/opt/netbeans/logotype1.png' >> /home/$USER/.local/share/applications/netbeans.desktop
 sudo wget https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Apache_NetBeans_Logo.svg/200px-Apache_NetBeans_Logo.svg.png -O /opt/netbeans/logotype1.png
 echo 'Netbeans installed'
+sudo apt clean
 exit 0
 else
 echo 'Checksum missmatch, please run the install script again.'
